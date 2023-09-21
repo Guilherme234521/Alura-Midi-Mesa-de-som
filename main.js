4636ef948c0b8fad9bc0f2927db0090d8a3acfa3
Guilherme234521 <112871023+Guilherme234521@users.noreply.github.com>
2023-09-20 22:43:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,43 +1,95 @@
-function tocaSom(seletorAudio) {
-  const elemento = document.querySelector(seletorAudio);
-  if (elemento && elemento.localName === "audio") {
-    elemento.play();
-  } else {
-    console.log("Elemento não encontrado ou seletor inválido");
-  }
-}
-//Constante para salvar lista de teclas e código para localizar essas teclas
-const listaDeTeclas = document.querySelectorAll(".tecla");
-
-//para
-for (let contador = 0; contador < listaDeTeclas.length; contador++) {
-  //Constante tecla para salvar a tecla clicada da listaDeTeclas
-  const tecla = listaDeTeclas[contador];
-
-  //Constante instrumento para salvar a segunda classe do elemento clicado da listaDeTeclas
-  //(*Contagem começa em 0)
-  const instrumento = tecla.classList[1];
-
-  //template string, com a função de criar a constante idAudio para para linkar a tecla clicada e o
-  //som que deve ser reproduzido.
-  const idAudio = `#som_${instrumento}`;
-
-  //Ao clicar, ativa a função tocaSom para tocar o som clicado
-  tecla.onclick = function () {
-    tocaSom(idAudio);
-  };
-
-  //funcinalidade com as teclas
-  //tecla pressionada
-  tecla.onkeydown = function (evento) {
-    // || significa ou
-    if (evento.code === "Enter" || evento.code === "Space") {
-      tecla.classList.add("ativa");
+const keySongs = [
+    {
+        key: 7,
+        song: 'pom',
+        down: false
+    },
+    {
+        key: 8,
+        song: 'clap',
+        down: false
+    },
+    {
+        key: 9,
+        song: 'tim',
+        down: false
+    },
+    {
+        key: 4,
+        song: 'puff',
+        down: false
+    },
+    {
+        key: 5,
+        song: 'splash',
+        down: false
+    },
+    {
+        key: 6,
+        song: 'toim',
+        down: false
+    },
+    {
+        key: 1,
+        song: 'psh',
+        down: false
+    },
+    {
+        key: 2,
+        song: 'tic',
+        down: false
+    },
+    {
+        key: 3,
+        song: 'tom',
+        down: false
     }
-  };
+]
 
-  //tecla solta
-  tecla.onkeyup = function () {
-    tecla.classList.remove("ativa");
-  };
+function tocaSom(audioElement) {
+    audioElement.currentTime = 0
+    audioElement.play()
+}
+
+keySongs.forEach(song => {
+    let keyElement = document.querySelector(`.tecla_${song.song}`)
+
+    song.keyElement = keyElement
+    song.audioElement = document.getElementById(`som_tecla_${song.song}`)
+
+    keyElement.addEventListener('click', () => {
+        tocaSom(song.audioElement)
+    })   
+
+})
+
+document.onkeydown = (event) => {
+    const key = event.key
+    const keySong =  keySongs.find(keySong => keySong.key == key)
+    
+    if(keySong) {
+
+        keySong.keyElement.classList.add('ativa')
+
+        if(!keySong.down) {
+            tocaSom(keySong.audioElement)
+        }
+    
+        keySong.down = true
+
+    }
+
+}
+
+document.onkeyup = (event) => {
+    const key = event.key
+    const keySong =  keySongs.find(keySong => keySong.key == key)
+    
+    if(keySong) {
+    
+        keySong.keyElement.classList.remove('ativa')
+        keySong.down = false
+
+    }
+
 }
